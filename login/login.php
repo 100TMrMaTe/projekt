df3ca7ef550af8c36affb59d858315aa9d5f79de
100TMrMaTe <konyhasi.mate.21@ady-nagyatad.hu>
2025-11-27 10:31:28 +0100

    updated

--- login/login.php
+++ login/login.php
@@ -3,24 +3,33 @@ $conn = Connect_to_serer();
 Create_databse_tables($conn);
 Use_database($conn, 'projekt');
 $users = Select_from($conn, 'users');
+
 function redirect($url)
 {
     header('Location: ' . $url);
     die();
 }
+
 if (isset($_POST["email"]) && isset($_POST["password"])) {
     $keres_email = $_POST["email"];
     $keres_password = $_POST["password"];
+    
     if ($keres_email == "nemeth.kristof@ady-nagyatad.hu" && $keres_password == "admin") {
         redirect("http://localhost/suliscucc/projekt/adminwepage/adminpage.php");
     }
 
+    $sikeres_bejelentkezes = false;
+    
     foreach ($users as $row) {
         if ($keres_email == $row[2] && $keres_password == $row[4]) {
+            $sikeres_bejelentkezes = true;
             redirect("http://localhost/suliscucc/projekt/music/music.php");
         }
     }
-    echo "<script type='text/javascript'>alert('valami nem jo gec');</script>";
-    redirect("C:\xampp\htdocs\suliscucc\projekt\login\login.html");
+    
+    if (!$sikeres_bejelentkezes) {
+        redirect("http://localhost/suliscucc/projekt/login/login.html?error=1");
+    }
 }
+
 Kill_server_connection($conn);
