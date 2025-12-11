b0f8102046b8f26b16ca37bd7afb72bd00ada8eb
100TMrMaTe <konyhasi.mate.21@ady-nagyatad.hu>
2025-12-11 12:25:22 +0100

    update

--- database.php
+++ database.php
@@ -3,10 +3,10 @@ function Connect_to_server()
 {
   $servername = "172.16.2.100";
   $username = "root";
-  $password = "";
+  $password = "admin";
 
   // Create connection
-  $conn = mysqli_connect($servername, $username, $password);
+  $conn = mysqli_connect($servername, $username, $password, "", 3306);
   // Check connection
   if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
