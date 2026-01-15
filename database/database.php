aa0ecf45c66e5b0ef96a05edb5e267ced1b87cf0
100TMrMaTe <konyhasi.mate.21@ady-nagyatad.hu>
2026-01-15 12:26:27 +0100

    updated

--- database/database.php
+++ database/database.php
@@ -1,4 +1,6 @@
 <?php
+
+include_once __DIR__ . "/../mailer/sendVerification.php";
 function Connect_to_server()
 {
   $servername = "172.16.2.100";
@@ -17,36 +19,36 @@ function Connect_to_server()
 }
 function registration($conn, $username, $password, $email, $class)
 {
-  $usersql = "SELECT * FROM users WHERE email='$email'";
-
-
   $password = password_hash($password, PASSWORD_BCRYPT);
   $username = mysqli_real_escape_string($conn, $username);
   $email = mysqli_real_escape_string($conn, $email);
-  if(str_ends_with($email, "@ady-nagyatad.hu") == false){
+  $class = mysqli_real_escape_string($conn, $class);
+  $token = bin2hex(random_bytes(32));
+
+  if (str_ends_with($email, "@ady-nagyatad.hu") == false) {
     $vissza["status"] = "error";
-    $vissza["message"] = "Csak sulis email cimmel lehet regisztralni!"; 
-    echo json_encode($vissza);
-    return;
-  }else if(mysqli_query($conn, $usersql)->num_rows > 0){
-    $vissza["status"] = "error";
-    $vissza["message"] = "Az email mar foglalt!"; 
-    $vissza["longmessage"] = "Amennyiben nem te regisztraltal, kerlek vedd fel a kapcsolatot a rendszergazdaval!";
+    $vissza["message"] = "Csak sulis email cimmel lehet regisztralni!";
     echo json_encode($vissza);
     return;
   }
-  $class = mysqli_real_escape_string($conn, $class);
 
 
+  mysqli_report(MYSQLI_REPORT_OFF);
 
-  $sql = "INSERT INTO users (user_name, email, user_class, user_password) VALUES ('$username', '$email', '$class', '$password')";
+  $sql = "INSERT INTO users (user_name, email, user_class, user_password,verification_token) VALUES ('$username', '$email', '$class', '$password', '$token')";
   if (mysqli_query($conn, $sql)) {
     $vissza["status"] = "success";
     $vissza["message"] = "Sikeres regisztracio!";
+    $vissza["longmessage"] = "Kerlek ellenorizd az emailed a megerosito linkert!";
+    echo json_encode($vissza);
+    sendVerificationEmail($email, $token);
   } else {
     $vissza["status"] = "error";
-    $vissza["message"] = "Hiba a regisztracio soran probald ujra!";
-    $vissza["errormessage"] = mysqli_error($conn);
+    $vissza["message"] = "Az email mar foglalt!";
+    //link forgot message
+    //$vissza["errormessage"] = mysqli_error($conn);
+    echo json_encode($vissza);
+    return;
   }
-  echo json_encode($vissza);
+  mysqli_report(MYSQLI_REPORT_ALL); 
 }
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 include_once "database/database.php";
-
 $conn = Connect_to_server();
 $data = (json_decode(file_get_contents("php://input"), true));
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
@@ -14,4 +13,15 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     }
 }
 
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+    if (isset($_GET["email_token"])) {
+        $token = $_GET["email_token"];
+        $sql = "UPDATE users SET email_verified = 1 WHERE verification_token = '$token'";
+        if (mysqli_query($conn, $sql)) {
+            echo "Az email címed sikeresen megerősítve!";
+        } else {
+            echo "Hiba történt az email cím megerősítése során."; 
+        }
+    }
+}
 mysqli_close($conn);
